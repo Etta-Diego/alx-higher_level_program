09c495529961157d83b4132c53bbd5733b3e18f8
Etta-Diego <onyinyechiikediego.gmail.com>
2023-11-28 23:12:05 +0100

    more update on task 4 -6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{:02d}, ".format(i * 10 + j), end="")
+        if i != 8:
+            print("{:02d}, ".format(i * 10 + j), end="")
+        else:
+            print("{:02d} ".format(i * 10 + j), end="\n")
