ab96c681909893cb6d5a3d1f9643018bf446f850
Etta-Diego <onyinyechiikediego.gmail.com>
2023-11-29 17:03:02 +0100

    Fixed task 8 pycodestyle, update task 9, 10, 11, 12, 14

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,8 @@ def uppercase(str):
     new_str = ""
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
-            upper_c  = ord(c) - 32
+            upper_c = ord(c) - 32
         else:
-           upper_c = ord(c)
+            upper_c = ord(c)
         new_str += chr(upper_c)
-#print("{}".formai(chr(new_str)), end="")
     print("{}".format(new_str))
