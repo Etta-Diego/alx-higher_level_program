91776f38bd949ec68831a1ccff7575d27855ed65
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-01 00:35:06 +0100

    refixed spelling error in task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    if (len(sys.argv) - 1) > 0:
-        print("{} arguments:".format(len(sys.argv) - 1))
+    if (len(sys.argv) - 1) == 1:
+        print("{} argument:".format(len(sys.argv) - 1))
     else:
         print("{} arguments.".format(len(sys.argv) - 1))
     for i in range(1, len((sys.argv))):
