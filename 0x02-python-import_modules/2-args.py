689fac00a8410d5f77f6e017ac97e6e1a9ae1864
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-01 00:48:42 +0100

    refixed requirement error in task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@ import sys
 if __name__ == "__main__":
     if (len(sys.argv) - 1) == 1:
         print("{} argument:".format(len(sys.argv) - 1))
-    else:
+    elif (len(sys.argv) - 1) == 0:
         print("{} arguments.".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len((sys.argv))):
-        print("{} {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
