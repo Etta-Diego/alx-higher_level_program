3024c65c4fd05a96c3694f173247b6c3d1ce6a3d
Etta-Diego <onyinyechiikediego.gmail.com>
2023-11-30 20:47:45 +0100

    first commit for task 0 - 3

---
+++ 0x02-python-import_modules/1-calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    plus = add(a, b)
+    diff = sub(a, b)
+    multiply = mul(a, b)
+    division = div(a, b)
+    print("{} + {} = {}".format(a, b, plus))
+    print("{} - {} = {}".format(a, b, diff))
+    print("{} * {} = {}".format(a, b, multiply))
+    print("{} / {} = {}".format(a, b, division))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+args_sum = 0
+
+for arg in sys.argv[1:]:
+    args_sum += int(arg)
+
+print(args_sum)
