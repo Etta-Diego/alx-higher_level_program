ecd3dc0f54c47549d313fec43312c0ea2fb2622a
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-01 01:08:09 +0100

    refixed requirement error in task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
+length = len(sys.argv
 args_sum = 0
 for arg in sys.argv[1:]:
-    args_sum += int(argv[arg])
+    args_sum += int(arg)
 print("{:d}".format(args_sum))
