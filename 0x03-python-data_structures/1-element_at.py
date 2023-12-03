4b71724c7c6c5e8a9528410e02d7f3db66ca061b
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-03 21:41:38 +0100

    task 1 update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     length = len(my_list)
     if idx < 0:
         return None
-    if idx > length:
+    if idx >= length:
         return None
     for i in range(length):
         return my_list[idx]
