83dd6f019d89a11c61825ae6c608f1c4a7804162
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-04 22:56:45 +0100

    task 12 update

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return None
+    for num in range(len(my_list)):
+        if my_list[num] % 2 == 0:
+            return 
