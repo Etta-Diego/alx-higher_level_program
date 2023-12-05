5f3ddc8d70d6fb3cb17c574415a2116e45d3a6f8
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-05 13:12:25 +0100

    done with task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is None:
+        return [num % 2 == 0 for num in my_list]
+
+    """
+   # my_lists = my_list
+   # length = len(my_lists)
+    if not my_lists:
         return None
-    for num in range(len(my_list)):
-        if my_list[num] % 2 == 0:
-            return 
+#    for num in range(0, length):
+        if my_lists[num] % 2 == 0:
+           return my_lists[num] % 2, True
+        else:
+            return my_lists, False
+        """
