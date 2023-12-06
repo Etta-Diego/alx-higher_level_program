89dd0454898d6472ca470d5c6995e9dcd7b4f75c
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-06 19:31:37 +0100

    task 7 correction 0

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    update = a_dictionary[key]
-    update = value
-    return update
-    #for keys in a_dictionary:
-     #   print("{}: {}".format(keys, a_dictionary[keys]))
+    #a_dictionary[key] = value
+    a_dictionary.update(key = value)
+    return a_dictionary
