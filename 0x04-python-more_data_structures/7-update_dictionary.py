c29da23b091fcc1b906c0a003535d1383bb88634
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-06 19:17:39 +0100

    task 6 and 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    update = a_dictionary[key]
+    update = value
+    return update
+    #for keys in a_dictionary:
+     #   print("{}: {}".format(keys, a_dictionary[keys]))
