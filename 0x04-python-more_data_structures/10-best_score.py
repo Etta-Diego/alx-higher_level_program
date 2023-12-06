dcc272b093cdc31df4b769fc92e619a642a9aba9
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-06 23:17:50 +0100

    task 10 correction 2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     for key, value in a_dictionary.items():
-            if value > max_value:
-                max_value = value
-                max_key = key
+        if value > max_value:
+            max_value = value
+            max_key = key
     return max_key
