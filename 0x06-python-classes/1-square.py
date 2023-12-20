1a3e28a9872cb5a0de48162135dfad00a1c53e23
Etta-Diego <onyinyechiikediego.gmail.com>
2023-12-20 22:36:20 +0100

    task 1 documentation correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,12 +5,15 @@
 
 
 class Square:
-   """ A class Square that defines a square
+    """
+    A class Square that defines a square
     Attributes:
     __size(int): The square's size
+
     """
     def __init__(self, size):
-        """Initializes a new instance of square.
+        """
+        Initializes a new instance of square.
 
         Args:
         size(int): The size of the square
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,16 +5,18 @@
 
 
 class Square:
-""" A class Square that defines a square
+    """
+    A class Square that defines a square
     Attributes:
     __size(int): The square's size
-   """
-    def __init__(self, size):
-        """Initializes a new instance of square.
 
-        Args:
-        size(int): The size of the square
-        """
+    """
+
+    def __init__(self, size):
+        
+        # Initializes a new instance of square.
+        # Args:
+        # size(int): The size of the square
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
